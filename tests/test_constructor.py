5e454b17b3a58b64fbefad1bdd5cad81aee38510
k-dominik <k-dominik@users.noreply.github.com>
2019-04-16 11:25:36 +0200

    refactor: simplify test setup

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -10,9 +10,7 @@ import string
 import typing
 
 
-@pytest.fixture
-def random_data() -> bytes:
-    random_str_len = 2000
+def generate_random_data(random_str_len=2000) -> bytes:
     random_data = bytes(
         "".join([chr(random.randint(0, 1114111)) for x in range(random_str_len)]),
         "utf-32",
@@ -28,90 +26,57 @@ def inserted_to_binary(data: bytes, repl: bytes, start_pos: int) -> bytes:
     return data[0:start_pos] + repl + b"\0" + data[start_pos + len(repl) + 1 : :]
 
 
+def random_data_w_prefix(
+    random_str_len: int, original_prefix: bytes, current_prefix: bytes, occurrences: int
+) -> bytes:
+    data = generate_random_data(random_str_len)
+    len_diff = len(original_prefix) - len(current_prefix)
+    bin_str = current_prefix + b"\0" * (len_diff + 1)
+    assert occurrences * len(bin_str) < random_str_len
+    assert data.count(original_prefix) == 0
+    assert data.count(current_prefix) == 0
+    interval_length = random_str_len // occurrences + 1
+    for i in range(0, random_str_len, interval_length):
+        start_pos = random.randint(i, i + interval_length - len(bin_str))
+        data = inserted_to_binary(data, bin_str, start_pos)
+
+    assert data.count(current_prefix) == occurrences
+    return data
+
+
 def zero_t(data: bytes) -> bytes:
     return data + b"\0"
 
 
 @pytest.mark.parametrize(
-    "original_prefix,current_prefix, new_prefix",
+    "original_prefix,current_prefix, new_prefix,occurrences",
     [
-        (b"this_is_a_short_placeholder", b"short_current", b"short_new"),
-        (b"AAAAA", b"BBBBB", b"CCCCC"),
-        (b"AAAAA", b"BBB", b"CC"),
-        (b"AAAAA", b"BBB", b"CCCCC"),
+        (b"this_is_a_short_placeholder", b"short_current", b"short_new", 1),
+        (b"AAAAA", b"BBBBB", b"CCCCC", 7),
+        (b"AAAAA", b"BBB", b"CC", 1),
+        (b"AAAAA", b"BBB", b"CCCCC", 12),
     ],
 )
-def test_single_replace(
-    original_prefix: bytes, current_prefix: bytes, new_prefix: bytes, random_data: bytes
+def test_replace(
+    original_prefix: bytes, current_prefix: bytes, new_prefix: bytes, occurrences: int
 ):
-    random_str_len = len(random_data)
-    assert random_data.count(original_prefix) == 0
-    # insert the original prefix
-    start_pos = random.randint(0, random_str_len - len(original_prefix) - 1)
-    random_data = inserted_to_binary(random_data, original_prefix, start_pos)
-    assert random_data.count(original_prefix) == 1
-    # insert the current prefix
-    random_data = re.sub(
-        re.escape(original_prefix) + b"\0",
-        current_prefix + b"\0" * (len(original_prefix) - len(current_prefix) + 1),
-        random_data,
+    random_str_len = 2000
+    random_data = random_data_w_prefix(
+        random_str_len, original_prefix, current_prefix, occurrences
     )
-    assert current_prefix in random_data
     res = _constructor.binary_replace(
         random_data, original_prefix, current_prefix, new_prefix
     )
-    assert new_prefix in res
+    assert res.count(new_prefix) == occurrences
 
 
 @pytest.mark.parametrize(
-    "original_prefix,current_prefix, new_prefix",
+    "nested_with,original_prefix,current_prefix,new_prefix,occurrences",
     [
-        (b"this_is_a_short_placeholder", b"short_current", b"short_new"),
-        (b"AAAAA", b"BBBBB", b"CCCCC"),
-        (b"AAAAA", b"BBBB", b"CCC"),
-        (b"AAAAA", b"BBB", b"CCCCC"),
-    ],
-)
-def test_multi_replace(
-    original_prefix: bytes, current_prefix: bytes, new_prefix: bytes, random_data: bytes
-):
-    random_str_len = len(random_data)
-    assert random_data.count(original_prefix) == 0
-    # insert the original prefix
-    start_pos = random.randint(0, random_str_len // 2 - len(original_prefix) - 1)
-    random_data = inserted_to_binary(random_data, original_prefix, start_pos)
-    assert random_data.count(original_prefix) == 1
-
-    # n+1: insert the original prefix
-    start_pos = random.randint(
-        random_str_len // 2, random_str_len - len(original_prefix) - 1
-    )
-    random_data = inserted_to_binary(random_data, original_prefix, start_pos)
-    assert random_data.count(original_prefix) == 2
-
-    # insert the current prefix
-    random_data = re.sub(
-        re.escape(original_prefix) + b"\0",
-        current_prefix + b"\0" * (len(original_prefix) - len(current_prefix) + 1),
-        random_data,
-    )
-    assert random_data.count(zero_t(original_prefix)) == 0
-    assert random_data.count(zero_t(current_prefix)) == 2
-
-    res = _constructor.binary_replace(
-        random_data, original_prefix, current_prefix, new_prefix
-    )
-    assert res.count(zero_t(new_prefix)) == 2
-    assert random_data.count(zero_t(original_prefix)) == 0
-
-
-@pytest.mark.parametrize(
-    "nested_with,original_prefix,current_prefix, new_prefix",
-    [
-        ((b"",), b"this_is_a_short_placeholder", b"short_current", b"short_new"),
-        ((b"/something/here",), b"AAAAA", b"BBB", b"CCC"),
-        ((b"/one/here", b"/other/one/here/yo"), b"AAAAA", b"BBB", b"CC"),
-        ((b"/one/here", b"/other/one/here/yo"), b"AAAAA", b"BBB", b"CCCCC"),
+        ((b"",), b"this_is_a_short_placeholder", b"short_current", b"short_new", 1),
+        ((b"/something/here",), b"AAAAA", b"BBB", b"CCC", 10),
+        ((b"/one/here", b"/other/one/here/yo"), b"AAAAA", b"BBB", b"CC", 11),
+        ((b"/one/here", b"/other/one/here/yo"), b"AAAAA", b"BBB", b"CCCCC", 7),
     ],
 )
 def test_multi_nested_replace(
@@ -119,34 +84,18 @@ def test_multi_nested_replace(
     original_prefix: bytes,
     current_prefix: bytes,
     new_prefix: bytes,
-    random_data: bytes,
+    occurrences: int,
 ):
-    assert random_data.count(original_prefix) == 0
-    random_str_len = len(random_data)
+    random_str_len = 2000
     original_nested = b"".join([original_prefix + y for y in nested_with])
-    repl_string_len = len(original_nested)
-
-    # insert the original prefix
-    start_pos = random.randint(0, random_str_len // 2 - repl_string_len - 1)
-    random_data = inserted_to_binary(random_data, original_nested, start_pos)
-    assert random_data.count(original_prefix) == len(nested_with)
-
-    # n+1: insert the original prefix
-    start_pos = random.randint(
-        random_str_len // 2, random_str_len - repl_string_len - 1
-    )
-    random_data = inserted_to_binary(random_data, original_nested, start_pos)
-    assert random_data.count(original_prefix) == 2 * len(nested_with)
-
     current_nested = original_nested.replace(original_prefix, current_prefix)
     current_nested = current_nested + b"\0" * (
         len(original_nested) - len(current_nested)
     )
-    assert len(current_nested) == len(original_nested)
-    # insert the current prefix
-    random_data = re.sub(re.escape(original_nested), current_nested, random_data)
-    assert random_data.count(original_prefix) == 0
-    assert random_data.count(current_prefix) == 2 * len(nested_with)
+
+    random_data = random_data_w_prefix(
+        random_str_len, original_nested, current_nested, occurrences
+    )
 
     res = _constructor.binary_replace(
         random_data, original_prefix, current_prefix, new_prefix
@@ -156,7 +105,7 @@ def test_multi_nested_replace(
 
     new_nested = current_nested.rstrip(b"\0").replace(current_prefix, new_prefix)
     new_nested = new_nested + b"\0" * (len(original_nested) - len(new_nested))
-    assert res.count(zero_t(new_nested)) == 2
+    assert res.count(zero_t(new_nested)) == occurrences
 
 
 @pytest.fixture
@@ -209,31 +158,32 @@ def test_update_prefix_text(new_prefix: str, text_file):
     assert txt_out.count(new_prefix) == 10
 
 
-@pytest.fixture
-def binary_file(
-    tmp_path: pathlib.Path, random_data: bytes
-) -> typing.Tuple[str, pathlib.Path]:
-    bin_file = tmp_path / "binfile.bin"
+@pytest.mark.parametrize(
+    "new_prefix,occurrences", [("1234", 5), ("1234567", 3), ("1234567890abcderfgh1", 1)]
+)
+def test_update_prefix_text_binary(
+    new_prefix: str, occurrences, tmp_path: pathlib.Path
+):
     original_prefix = "123456789_max_length"
-    prefix = "whatever"
-    len_diff = len(original_prefix) - len(prefix)
-    bin_data = inserted_to_binary(
-        random_data, zero_t(prefix.encode("utf-8")) + b"\0" * len_diff, 100
+    current_prefix = "whatever"
+    random_str_len = 2000
+    random_data = random_data_w_prefix(
+        random_str_len,
+        original_prefix.encode("utf-8"),
+        current_prefix.encode("utf-8"),
+        occurrences,
     )
 
-    with bin_file.open("wb") as f:
-        f.write(bin_data)
-    return prefix, bin_file
+    bin_path = tmp_path / "binfile.bin"
+    with bin_path.open("wb") as f:
+        f.write(random_data)
 
-
-@pytest.mark.parametrize("new_prefix", ["1234", "1234567", "1234567890abcderfgh1"])
-def test_update_prefix_text_binary(new_prefix, binary_file):
-    original_prefix: str = "123456789_max_length"
-    current_prefix, path = binary_file
     mode: str = "binary"
-    _constructor.update_prefix(path, original_prefix, current_prefix, new_prefix, mode)
+    _constructor.update_prefix(
+        bin_path, original_prefix, current_prefix, new_prefix, mode
+    )
 
-    with path.open("rb") as f:
+    with bin_path.open("rb") as f:
         bin_out = f.read()
 
-    assert bin_out.count(new_prefix.encode("utf-8")) == 1
+    assert bin_out.count(new_prefix.encode("utf-8")) == occurrences
